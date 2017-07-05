3c6603d485e84cd28d9b862e689183d3cc8750d2
Kevin Killick <caoimh@outlook.com>
2017-07-05 18:08:51 +0100

    Fixed Display Name Owner Model

--- TrailerCheck/Models/Owner.cs
+++ TrailerCheck/Models/Owner.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrailerCheck.Models
 {
     public class Owner
     {
         public int ID { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Registration Date")]
         public DateTime RegistrationDate { get; set; }
 
         public ICollection<Registration> Registrations { get; set; }
